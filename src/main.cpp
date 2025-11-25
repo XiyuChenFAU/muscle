f93ed1e6a61e9b75f855bbfd8916a3c96625061b
Xiyu Chen <xiyu.chen@fau.de>
2025-11-25 16:57:00 +0100

    run for both gui and terminal

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,45 @@ Xiyu Chen
 
 
 int main(int argc, char* argv[]){
-    
-    QApplication a(argc, argv);
-    MainWindow mainWin;
-    mainWin.show();
-    return a.exec();
-    
-   /*
-   runmodel Runmodel("../example/elbow_model.json",1);
-   Runmodel.runprogram();
-   return 1
-   */
+
+    if(argc > 1){
+        std::filesystem::path input_path = argv[1];
+        if (!std::filesystem::exists(input_path)) {
+            std::cerr << "Error: Path does not exist.\n";
+            return 1;
+        }
+
+        if (std::filesystem::is_regular_file(input_path) && input_path.extension() == ".json") {
+            runmodel muscle_model(input_path.string(), 1);
+            double t = muscle_model.runprogramm();
+            return 0;
+        }
+
+        if (std::filesystem::is_directory(input_path)) {
+            std::vector<std::filesystem::path> json_files;
+
+            for (const auto& entry : std::filesystem::directory_iterator(input_path)) {
+                if (std::filesystem::is_regular_file(entry) && entry.path().extension() == ".json") {
+                    json_files.push_back(entry.path());
+                }
+            }
+
+            if (json_files.empty()) {
+                std::cout << "No JSON files found in folder.\n";
+                return 0;
+            }
+
+            for (const auto& json_file : json_files) {
+                runmodel muscle_model(json_file.string(), 1);
+                double t = muscle_model.runprogramm();
+            }
+            return 0;
+        }
+    } else {
+        QApplication a(argc, argv);
+        MainWindow mainWin;
+        mainWin.show();
+        return a.exec();
+    }
 }
 
